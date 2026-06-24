4207da3afd545253e107fd3350479de591164385
kolya-zuz <donbass1161@gmail.com>
2026-06-24 12:59:26 +0300

    Add files via upload

--- Gym/slider.js
+++ Gym/slider.js
@@ -1,6 +1,36 @@
 const swiper = new Swiper('.swiper', {
   direction: 'vertical',
   loop: true,
-
   pagination: {
-    el: '.swiper-pagination',},});
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';},},
+     on: {
+        init: function () {
+            updatePagination(this);},
+
+        slideChange: function () {
+            updatePagination(this);}
+}});
+
+function updatePagination(swiper) {
+
+    let bullets = document.querySelectorAll('.swiper-pagination-bullet');
+
+    bullets.forEach((bullet, index) => {
+        bullet.innerHTML = '';
+
+        if (index === swiper.realIndex) {
+            bullet.innerHTML = index + 1;
+        }
+    });
+
+}
+
+
+    
+
+
+
+
