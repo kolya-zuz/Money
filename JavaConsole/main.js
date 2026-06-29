d6b1869e9ec77827d0ba14b9233e3dc63478ad52
kolya-zuz <donbass1161@gmail.com>
2026-06-29 18:30:07 +0300

    Add files via upload

---
+++ JavaConsole/main.js
@@ -0,0 +1,12 @@
+// Задача 1
+const firstName = "Tyler"
+const lastName = "Derden"
+const isStudent = true
+
+//Задача 2
+const age = 14
+currentYear = 2026
+birthYear = currentYear - age
+
+//Задача 3
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`)
